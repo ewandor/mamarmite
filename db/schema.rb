bc1245c115fb1bae19ac21008fb700dc2b8a9c44
Benjamin Mimran <mimranbenjamin@gmail.com>
2017-02-15 15:24:17 +0100

    je sais pas trop pk

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170215131808) do
 
   # These are extensions that must be enabled in order to support this database
@@ -56,11 +55,10 @@ ActiveRecord::Schema.define(version: 20170215131808) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
     t.string   "photos_location"
     t.string   "photo_cooker"
-
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_cooks_on_user_id", using: :btree
   end
 
