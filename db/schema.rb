5210fc7dfe5033cd4757929bd41613498f4b5e2e
Macintosh <liorbes@gmail.com>
2017-02-15 15:05:06 +0100

    index.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215112640) do
+ActiveRecord::Schema.define(version: 20170215113353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20170215112640) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_cooks_on_user_id", using: :btree
   end
 
