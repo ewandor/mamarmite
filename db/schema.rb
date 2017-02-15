0d89412a1940daefea332d2fd5499d380aa21e00
Elodie Royant <eroyant@gmail.com>
2017-02-15 16:22:48 +0100

    new seeeeeeeeeeeeeeeeeed

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170215131808) do
+ActiveRecord::Schema.define(version: 20170215150640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,17 +49,13 @@ ActiveRecord::Schema.define(version: 20170215131808) do
     t.integer  "capacity"
     t.string   "nickname"
     t.string   "motto"
-    t.string   "photos_recipes"
     t.integer  "price"
     t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "photos_location"
-    t.string   "photo_cooker"
-
     t.index ["user_id"], name: "index_cooks_on_user_id", using: :btree
   end
 
