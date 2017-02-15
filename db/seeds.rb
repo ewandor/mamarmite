b25b35b73ff78c6faa0a95cc1902f72509b7940c
Benjamin Mimran <mimranbenjamin@gmail.com>
2017-02-15 10:59:08 +0100

    Db seeds updated before merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,6 +32,4 @@ Cook.destroy_all
 
 Cook.create!(speciality: "French", address_street: "58 domaine saint-hugues", address_town: "Grenoble", zipcode: "38120", home_style: "House", age: 22, capacity: 5, nickname: "Le Gros Ju", motto: "Plus c'est gras plus c'est bon", price: 20)
 
-100.times  do
-  system("say jaime les bites")
-end
+
