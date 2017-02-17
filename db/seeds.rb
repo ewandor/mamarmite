42a7523a21084ced5c1de90a64c20a6840fde4c6
Ines Beatrix <ines.beatrix@essec.edu>
2017-02-17 14:30:11 +0100

    all pictures done

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,8 +71,8 @@ a = Cook.new(speciality: "French", address: "58 domaine saint-hugues, Paris", ho
 
 a.user = lyor
 a.recipe_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487094993/tajine-el-bey-800x600_suitnr.jpg"]
-a.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165784/Image_uploaded_from_iOS_ultabo.jpg"
-a.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169328/images-7_q5mc3f.jpg"]
+a.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487337189/French_woman_paqsob.jpg"
+a.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/r_0/v1487320938/k1_bbx6e6.jpg"]
 
 a.save!
 
@@ -82,17 +82,18 @@ b = Cook.new(speciality: "Italian", address: "33 rue des Francsbourgeois, Paris"
 
 b.user = ben
 b.recipe_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487094993/tajine-el-bey-800x600_suitnr.jpg"]
-b.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165780/Image_uploaded_from_iOS_1_dzakio.jpg"
-b.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169328/images-8_eycowv.jpg"]
+b.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487336426/Italian_woman_nhr9pv.jpg"
+b.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487320936/k2_xnx0xi.jpg"]
 b.save!
+
 
 c = Cook.new(speciality: "Moroccan", address: "4 rue des maroccains, Paris", home_style: "Appartment",
   age: 42, capacity: 2, nickname: "Z", motto: "On ne peut pas faire de cuisine si l'on aime pas les gens.", price: 30,
   )
 c.user = elodie
 c.recipe_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487094993/taboule_xxljev.png"]
-c.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165784/Image_uploaded_from_iOS_ultabo.jpg"
-c.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169328/imgres-3_g85tjg.jpg"]
+c.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487337310/Moroccan_woman_smhabl.png"
+c.location_photo_urls = ["https://cloudinary.com/console/media_library#/dialog/image/upload/k3_ci77rn "]
 c.save!
 
 
@@ -103,7 +104,7 @@ d = Cook.new(speciality: "Chinese", address: " 4 rue des petits champs, Paris", 
 d.user = ines
 d.recipe_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487094991/Harira_semoule_orge_au88k3.jpg"]
 d.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165780/Image_uploaded_from_iOS_1_dzakio.jpg"
-d.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169328/images-9_iuevae.jpg"]
+d.location_photo_urls = ["https://cloudinary.com/console/media_library#/dialog/image/upload/k4_qroktm"]
 d.save!
 
 e = Cook.new(speciality: "Indian", address: "6 rue des Petites-Écuries, Paris", home_style: "Appartment",
@@ -113,18 +114,18 @@ e = Cook.new(speciality: "Indian", address: "6 rue des Petites-Écuries, Paris",
 e.user = lyor
 e.recipe_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487157348/dohhzqatvs6ofocktz4b.jpg"]
 e.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165784/Image_uploaded_from_iOS_ultabo.jpg"
-e.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169328/images-6_ayswuz.jpg"]
+e.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487320936/k2_xnx0xi.jpg"]
 e.save!
 
 
 f = Cook.new(speciality: "Italian", address: "67 rue de l'ardoise, Paris", home_style: "Appartment",
-  age: 45, capacity: 2, nickname: "Gio", motto: "Bonne cuisine et bon vin, c'est le paradis sur terre.", price: 70,
+  age: 45, capacity: 2, nickname: "Giulia", motto: "Bonne cuisine et bon vin, c'est le paradis sur terre.", price: 70,
   )
 
 f.user = ben
 f.recipe_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487165197/Italian_3_a2tqvs.jpg"]
-f.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165784/Image_uploaded_from_iOS_ultabo.jpg"
-f.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169328/images-17_rp17np.jpg"]
+f.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487336425/Italian_woman_2_x4mk8b.jpg"
+f.location_photo_urls = ["https://cloudinary.com/console/media_library#/dialog/image/upload/k3_ci77rn "]
 f.save!
 
 g = Cook.new(speciality: "Indian", address: "120 avenue de Saint Exupery, Paris", home_style: "Appartment",
@@ -133,16 +134,16 @@ g = Cook.new(speciality: "Indian", address: "120 avenue de Saint Exupery, Paris"
 g.user = elodie
 g.recipe_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487164766/Indian_3_fvqb2o.jpg"]
 g.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165784/Image_uploaded_from_iOS_ultabo.jpg"
-g.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169328/images-4_rjbeaz.jpg"]
+g.location_photo_urls = ["https://cloudinary.com/console/media_library#/dialog/image/upload/k4_qroktm"]
 g.save!
 
-h = Cook.new(speciality: "French", address: "60 rue des Francsbourgeois, Paris", home_style: "Duplex",
-  age: 56, capacity: 12, nickname: "Albert", motto: "La cuisine est un moyen de faire plaisir à une personne.r", price: 40,
+h = Cook.new(speciality: "Italian", address: "60 rue des Francsbourgeois, Paris", home_style: "Duplex",
+  age: 56, capacity: 12, nickname: "Julietta", motto: "La cuisine est un moyen de faire plaisir à une personne.r", price: 40,
   )
 h.user = ines
 h.recipe_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487094992/saumon-en-croute-_piresi.jpg"]
-h.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165784/Image_uploaded_from_iOS_ultabo.jpg"
-h.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169328/images-5_af3fti.jpg"]
+h.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487337530/Italian_woman_4_efsjgr.jpg"
+h.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/r_0/v1487320938/k1_bbx6e6.jpg"]
 h.save!
 
 i = Cook.new(speciality: "Moroccan", address: "33 rue des petits fours, Paris", home_style: "Appartment",
@@ -150,8 +151,8 @@ i = Cook.new(speciality: "Moroccan", address: "33 rue des petits fours, Paris", 
   )
 i.user = lyor
 i.recipe_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487094991/kebbeh-epinards-noix-detc-800x600_hfuozr.png"]
-i.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165784/Image_uploaded_from_iOS_ultabo.jpg"
-i.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169327/images-13_fmhzmh.jpg"]
+i.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487337310/Moroccan_woman_smhabl.png"
+i.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487320936/k2_xnx0xi.jpg"]
 i.save!
 
 j = Cook.new(speciality: "Chinese", address: "81 rue Oberkampf, Paris", home_style: "Appartment",
@@ -159,18 +160,18 @@ j = Cook.new(speciality: "Chinese", address: "81 rue Oberkampf, Paris", home_sty
   )
 j.user = ben
 j.recipe_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487158979/images_mxxsss.jpg"]
-j.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165780/Image_uploaded_from_iOS_1_dzakio.jpg"
-j.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169327/images-16_j8gpu2.jpg"]
+j.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487336352/Chinese_woman_yasp04.jpg"
+j.location_photo_urls = ["https://cloudinary.com/console/media_library#/dialog/image/upload/k3_ci77rn "]
 j.save!
 
 k = Cook.new(speciality: "Italian", address: "111 rue du Faubourg Poissoniere, Paris", home_style: "Appartment",
-  age: 33, capacity: 2, nickname: "Giulia", motto: "Tout est plus facile à dire dans une cuisine, tout y est nuancé par cette intention du partage, un appétit fait de la sève même des choses.", price: 65,
+  age: 33, capacity: 2, nickname: "Gio", motto: "Tout est plus facile à dire dans une cuisine, tout y est nuancé par cette intention du partage, un appétit fait de la sève même des choses.", price: 65,
   )
 
 k.user = elodie
 k.recipe_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487164196/grid-cell-8256-1384892528-7_qq89yj.jpg"]
-k.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165784/Image_uploaded_from_iOS_ultabo.jpg"
-k.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169327/images-15_ilvgtq.jpg"]
+k.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487337189/French_woman_paqsob.jpg"
+k.location_photo_urls = ["https://cloudinary.com/console/media_library#/dialog/image/upload/k4_qroktm"]
 k.save!
 
 l = Cook.new(speciality: "Indian", address: "213 rue des grelons, Paris", home_style: "Appartment",
@@ -178,8 +179,8 @@ l = Cook.new(speciality: "Indian", address: "213 rue des grelons, Paris", home_s
   )
 l.user = ines
 l.recipe_photo_urls = ["https://cloudinary.com/console/media_library#/dialog/image/upload/imgres-1_vzfhgd"]
-l.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487165784/Image_uploaded_from_iOS_ultabo.jpg"
-l.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/v1487169327/images-10_xn1nxf.jpg"]
+l.cooker_photo_url = "http://res.cloudinary.com/mamarmite/image/upload/v1487337031/Indian_Woman_ywvepn.jpg"
+l.location_photo_urls = ["http://res.cloudinary.com/mamarmite/image/upload/r_0/v1487320938/k1_bbx6e6.jpg"]
 l.save!
 
 
